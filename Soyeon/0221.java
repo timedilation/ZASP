4ae8e8e06640dd6eb14a33ebeb1a244290bb233e
sypark9646 <46469385+sypark9646@users.noreply.github.com>
2019-03-01 15:26:20 +0900

    Update 0221.java

--- Soyeon/0221.java
+++ Soyeon/0221.java
@@ -1,7 +1,8 @@
 /** list마다 숫자가 써 있는데, 
  * list가 해당 칸에서 최대 점프할 수 있는 길이일 때, 
- * 첫 번째 -> 마지막 칸에 다다를수 있는지 : boolean toEnd(int[])
- * 첫 번째 -> 마지막 칸에 다다르는 최소의 점프 수: int minJump(int[])
+ * https://leetcode.com/problems/jump-game/
+ * 첫 번째 -> 마지막 칸에 다다를수 있는지 : boolean toEnd(int[]) --> O(N^2) --> O(N) 가능
+ * 첫 번째 -> 마지막 칸에 다다르는 최소의 점프 수: int minJump(int[]) --> O(N^4) --> O(N^2) 가능???
  **/
 import java.util.*;
 public class Main {
@@ -18,7 +19,7 @@ public class Main {
 		int reach[]=new int[length];// reach[i]: i번째 칸에 도달할 수 있으면 1, 없으면 0
 		
 		//시간 복잡도 O(N^2)
-       		reach[0]=1;
+        	reach[0]=1;
 		for(int idx=0;idx<length;idx++) {
 			//도달 할 수 있는 경우에만 체크해야 하는 것 빼먹지 말자
 			if(reach[idx]==1){// list에 원소 하나있을 때 주의
@@ -28,11 +29,23 @@ public class Main {
 			    }
             		}
 		}
+		
 		boolean isPossible=false;
 		if(reach[length-1]==1)
 			isPossible=true;
 		
 		return isPossible;
+	}
+	public static boolean toEnd2(int[] list){
+		//[2, 0, 1, 1, 2] -> true
+		//[1, 0, 2, 1, 3] -> false
+		int reachable = 0;
+		//시간 복잡도 O(N)
+    		for (int i=0; i<list.length; ++i) {
+        		if (i > reachable) return false;
+        		reachable = Math.max(reachable, i + list[i]);
+    		}
+    		return true;
 	}
 	public static int minJump(int[] list) {
 		int num=0;
@@ -41,7 +54,8 @@ public class Main {
 		int jumps[]=new int[length];// jumps[i]: i번째 칸에 도달하는 최소의 점프 수
 		Arrays.fill(jumps, -1);
 		
-		if(toEnd(list)) {
+		//시간복잡도 O(N^4)
+		if(toEnd(list)) {// 시간 복잡도 O(N^2)
 			//시간 복잡도 O(N^2)
 	        jumps[0]=num;
 	        reach[0]=1;
@@ -60,7 +74,7 @@ public class Main {
 					    	}
 				    	}
 				    }
-	            		}
+	            }
 			}
 		}
 		return jumps[length-1];
