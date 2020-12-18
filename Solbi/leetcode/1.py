bea03a532c3970680740af2951672bc646531859
solbi.choi2 <solbi.choi2@navercorp.com>
2020-12-18 23:31:01 +0900

    solve problem 1

---
+++ Solbi/leetcode/1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = {}
+        for i, x in enumerate(nums):
+            if target-x in d:
+                return [i, d[target-x]]
+            d[x] = i
