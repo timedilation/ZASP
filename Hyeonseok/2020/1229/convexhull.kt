a31d3c39b2980dad6c56404da673ac2b5ccfde48
Hyeonseok Choi <guardian1248@snu.ac.kr>
2020-12-29 23:58:38 +0900

    Create n list of dots

--- Hyeonseok/2020/1229/convexhull.kt
+++ Hyeonseok/2020/1229/convexhull.kt
@@ -1,8 +1,37 @@
+import java.util.Random
+
+class Dot {
+	var x: Int
+	var y: Int
+	init {
+		x = 0
+		y = 0
+	}
+}
+
 fun main(args: Array<String>) {
 	if (args.size < 1) {
 		println("usage: java -jre convexhull.jre [number of dots]")
 		return
 	}
 	val numDots: Int = args[0].toInt()
-	println(numDots)
-} 
+
+	var dotArray: Array<Dot> = CreateDots(numDots)
+	println(numDots.toString() + " dots created:")
+	for (i in 0..numDots-1) {
+		println("[" + dotArray[i].x + ", " + dotArray[i].y + "]")
+	}
+
+
+}
+
+fun CreateDots(num: Int) : Array<Dot> {
+	var dots : Array<Dot> = Array<Dot>(num, {Dot()});
+	val xList = (0..num*4).shuffled().take(num)
+	val yList = (0..num*4).shuffled().take(num)
+	for (i in 0..num-1) {
+		dots[i].x = xList[i]
+		dots[i].y = yList[i]
+	}
+	return dots
+}
