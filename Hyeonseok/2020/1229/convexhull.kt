125982cfd807f1cff5041e7d9e7743bca8b6127f
Hyeonseok Choi <guardian1248@snu.ac.kr>
2020-12-31 08:22:52 +0900

    Complete convexhull alrorithm

--- Hyeonseok/2020/1229/convexhull.kt
+++ Hyeonseok/2020/1229/convexhull.kt
@@ -7,7 +7,16 @@ class Dot {
 		x = 0
 		y = 0
 	}
+
+	fun equals(other: Dot) : Boolean {
+		return (this.x == other.x && this.y == other.y)
+	}
 }
+
+
+class Line (var start: Dot, var end: Dot){
+}
+
 
 fun main(args: Array<String>) {
 	if (args.size < 1) {
@@ -22,8 +31,13 @@ fun main(args: Array<String>) {
 		println("[" + dotArray[i].x + ", " + dotArray[i].y + "]")
 	}
 
-
+	val lineList = convexhull(dotArray)
+	println(lineList.size.toString() + " lines created:")
+	for (line in lineList) {
+		println("[" + line.start.x + ", " + line.start.y + "], [" + line.end.x + ", " + line.end.y + "]")
+	}
 }
+
 // create Array of Dot, with x coordinate sorted with ascending order
 fun CreateDots(num: Int) : Array<Dot> {
 	var dots : Array<Dot> = Array<Dot>(num, {Dot()});
@@ -35,4 +49,63 @@ fun CreateDots(num: Int) : Array<Dot> {
 		dots[i].y = yArray[i]
 	}
 	return dots
+}
+
+fun convexhull(dots: Array<Dot>): MutableList<Line> {
+	val dotsPair = separatePoints(dots, dots[0], dots[dots.size-1])
+	val lineList: MutableList<Line> = mutableListOf()
+
+	lineList.addAll(upperhull(dotsPair.first, dots[0], dots[dots.size-1]))
+	lineList.addAll(upperhull(dotsPair.second, dots[dots.size-1], dots[0]))
+
+	return lineList
+}
+
+fun separatePoints(dots: Array<Dot>, start: Dot, end: Dot) : Pair<MutableList<Dot>, MutableList<Dot>>{
+	var upper: MutableList<Dot> = mutableListOf()
+	var lower: MutableList<Dot> = mutableListOf()
+
+	for (dot in dots) {
+		if (start.equals(dot) || end.equals(dot)) continue
+		if (relPosition(start, end, dot) > 0) upper.add(dot)
+		else lower.add(dot)
+	}
+
+	return Pair(upper, lower)
+}
+
+fun relPosition(start: Dot, end: Dot, point: Dot): Int {
+	var a = start.y - end.y
+	var b = end.x - start.x
+	var c = start.x * end.y - start.y * end.x
+	return a * point.x + b * point.y + c
+}
+
+fun getDistance(start: Dot, end: Dot, point:Dot): Int {
+	return Math.abs(relPosition(start, end, point))
+}
+
+fun upperhull(dots: MutableList<Dot>, start: Dot, end: Dot) : MutableList<Line> {
+		var lineList: MutableList<Line> = mutableListOf()
+		if (dots.isEmpty()) {
+			lineList.add(Line(start, end))
+			return lineList
+		}
+		
+		var maxlen: Int = Int.MIN_VALUE
+		var maxpoint: Dot? = null
+		for (dot in dots) {
+			val dist = getDistance(start, end, dot)
+			if (dist > maxlen) {
+				maxlen = dist
+				maxpoint = dot
+			}
+		}
+
+		var dotsPair = separatePoints(dots.toTypedArray(), start, maxpoint!!)
+		lineList.addAll(upperhull(dotsPair.first, start, maxpoint))
+		dotsPair = separatePoints(dots.toTypedArray(), maxpoint, end)
+		lineList.addAll(upperhull(dotsPair.first, maxpoint, end))
+
+		return lineList
 }
