0b9276e204beb7c3c29965ec7d36a64c1275f250
pullup-dip-burpee <qazxdrtgbnj@gmail.com>
2021-02-09 14:55:52 +0900

    Modify 2294 faster

--- Euncheon/Baekjoon/DP/2294.cpp
+++ Euncheon/Baekjoon/DP/2294.cpp
@@ -9,16 +9,14 @@ int main(void){
     int n, k;
     scanf("%d %d", &n, &k);
     
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &coin[i]);
-    }
-    for (int i = 1; i < 10001; i++){
+    for (int i = 1; i <= k; i++){
         dp[i] = 987654321;
     }
     dp[0] = 0;
 
     for (int i = 0; i < n; i++) {
-        for (int j = 1; j <= k; j++) {
+        scanf("%d", &coin[i]);
+        for (int j = coin[i]; j <= k; j++) {
             if(coin[i] <= j) {
                 dp[j] = (dp[j] <=  dp[j-coin[i]] + 1) ? dp[j] : dp[j-coin[i]]+1;
             }
